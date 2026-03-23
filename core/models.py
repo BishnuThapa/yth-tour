c31ab2aca68da0e953d5622bb6d519eacc7bf752
Bishnu <thapabishnu20@gmail.com>
2026-03-23 21:16:18 +0545

    add tour model

--- core/models.py
+++ core/models.py
@@ -139,3 +139,86 @@ class Execlude(models.Model):
     class Meta:
         verbose_name = "Exclude"
         verbose_name_plural = "Excludes"
+
+
+
+class Tour(models.Model):
+    DIFFICULTY_CHOICES = (
+        ('Low', 'Low'),
+        ('Moderate', 'Moderate'),
+        ('High', 'High')
+    )
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
+    image = models.ImageField(upload_to='tour')
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE,related_name='tours')
+    duration = models.IntegerField(null=True, blank=True)
+    difficulty = models.CharField(
+        choices=DIFFICULTY_CHOICES, max_length=50, default='Low', null=True, blank=True)
+    max_altitude = models.CharField(max_length=100, null=True, blank=True)
+    best_season = models.CharField(max_length=100, null=True, blank=True)
+    accomodation = models.CharField(max_length=100, null=True, blank=True)
+    food = models.CharField(max_length=100, null=True, blank=True)
+    transpotation = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    starting_date = models.DateField(null=True, blank=True)
+    group_size = models.CharField(max_length=100, null=True, blank=True)
+    regular_price = models.IntegerField(null=True, blank=True)
+    sell_price = models.IntegerField(null=True, blank=True)
+    activity = models.ForeignKey(
+        Activity, on_delete=models.CASCADE, null=True, blank=True)
+    style = models.ManyToManyField(Style, null=True, blank=True)
+
+    overview = CKEditor5Field(config_name='extends')
+    includes = models.ManyToManyField(Include, null=True, blank=True)
+    excludes = models.ManyToManyField(Execlude,null=True, blank=True)
+    route_map = models.ImageField(upload_to='tour/map', null=True, blank=True)
+    promotional_video = models.URLField(null=True, blank=True, help_text="Enter YouTube video URL")
+
+    # itinerary = CKEditor5Field(config_name='extends',null=True, blank=True)
+    gear_check_list = CKEditor5Field(config_name='extends',null=True, blank=True)
+    
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Package'
+        verbose_name_plural = 'Packages'
+
+    def get_percentage(self):
+        new_price = ((self.regular_price-self.sell_price)*100) / \
+            self.regular_price
+        return new_price
+    
+     # ✅ Convert YouTube link to embeddable iframe URL
+    def get_embed_url(self):
+        if self.promotional_video:
+            if "watch?v=" in self.promotional_video:
+                return self.promotional_video.replace("watch?v=", "embed/")
+            elif "youtu.be/" in self.promotional_video:
+                return self.promotional_video.replace("youtu.be/", "www.youtube.com/embed/")
+            return self.promotional_video
+        return None
+        
+     # Resized JPEG (fallback)
+    image_optimized = ImageSpecField(
+        source='image',
+        processors=[ResizeToFit(1200)],
+        format='JPEG',
+        options={'quality': 80}
+    )
+
+    # WebP optimized version
+    image_webp = ImageSpecField(
+        source='image',
+        processors=[ResizeToFit(1200), Convert('WEBP')],
+        format='WEBP',
+        options={'quality': 80}
+    )
+
